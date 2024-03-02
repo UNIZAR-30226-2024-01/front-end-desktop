144ccb6f5bf003a23d73efef55e806703035bed8
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-03-02 19:23:13 +0100

    Aplicar desplegable en tarjeta

--- cluedo/src/app/tarjeta/tarjeta.component.ts
+++ cluedo/src/app/tarjeta/tarjeta.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DesplegableComponent } from '../desplegable/desplegable.component';
 import { TableCellComponent } from '../table-cell/table-cell.component';
 import { TableHeadComponent } from '../table-head/table-head.component';
 import { TableHeaderCellComponent } from '../table-header-cell/table-header-cell.component';
@@ -8,17 +9,17 @@ import { TableRowComponent } from '../table-row/table-row.component';
 @Component({
   selector: 'app-tarjeta',
   standalone: true,
-  imports: [CommonModule, TableCellComponent, TableHeadComponent, TableHeaderCellComponent, TableRowComponent],
+  imports: [CommonModule, 
+            TableCellComponent, TableHeadComponent, TableHeaderCellComponent, TableRowComponent,
+            DesplegableComponent],
   templateUrl: './tarjeta.component.html',
   styleUrl: '../../../../../front-end-shared/css/Tarjeta.css'
 })
 export class TarjetaComponent {
-  desplegable: boolean = false;
-  
-  style = { right: this.desplegable ? '0px' : '-20%' };
+  desplegado: boolean = false;
 
-    toggleDesplegable() {
-    this.desplegable = !this.desplegable;
-    this.style = { right: this.desplegable ? '0px' : '-20%' };
+  // Método para cambiar el estado de la variable "desplegado" para desplazar el chat
+  toggleDesplegado() {
+    this.desplegado = !this.desplegado;
   }
 }
