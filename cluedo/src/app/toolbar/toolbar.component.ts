16ec2dadf8caf6b1f9be411b3ea85b05b10a4ce7
Ignacio Millan <841537@unizar.es>
2024-03-07 12:11:29 +0100

    empezado menu no va del todo bien el boton

--- cluedo/src/app/toolbar/toolbar.component.ts
+++ cluedo/src/app/toolbar/toolbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule],
+  imports: [MatToolbarModule,MatIconModule,MatMenuModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
