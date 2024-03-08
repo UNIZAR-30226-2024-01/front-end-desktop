9d8653bee8d5a27b93ed3aa140e1bf736ef3dbb2
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-03-08 17:11:23 +0100

    Crear componentes login-page y game-page

--- cluedo/src/app/app.routes.ts
+++ cluedo/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginPageComponent },
+    { path: 'game-page', component: GamePageComponent }
+];
