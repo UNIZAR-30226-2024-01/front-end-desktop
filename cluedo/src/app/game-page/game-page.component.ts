1aff8471a743791520f78b37688c7fc45bb4d7c7
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-03-11 09:54:14 +0100

    Crear rutas login-page y game-page

--- cluedo/src/app/game-page/game-page.component.ts
+++ cluedo/src/app/game-page/game-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TarjetaComponent } from '../tarjeta/tarjeta.component';
+import { ChatComponent } from '../chat/chat.component';
+import { LoginPageComponent } from '../login-page/login-page.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-game-page',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,
+    LoginPageComponent,
+    GamePageComponent,
+    ChatComponent,
+    TarjetaComponent, ToolbarComponent],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.css'
 })
 export class GamePageComponent {
-
+  title = 'cluedo';
 }
