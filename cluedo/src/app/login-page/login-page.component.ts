cd3cea895091df69f7df77028eaa839621f3ad75
Pablo Moreno <841972@unizar.es>
2024-03-12 13:14:01 +0100

    conex con api angular

--- cluedo/src/app/login-page/login-page.component.ts
+++ cluedo/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: '../../../../../front-end-shared/css/Login/Login.css'
 })
 export class LoginPageComponent {
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string) {
+    this.http.post('http://localhost:3000/login', { username, password }).subscribe((res) => {
+      console.log(res);
+    });
+  }
+  
 
 }
