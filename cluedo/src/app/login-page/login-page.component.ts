05c9ae006e3d4d963bbdaa716c11e345e3148f5c
Pablo Moreno <841972@unizar.es>
2024-03-14 12:22:21 +0100

    correccion

--- cluedo/src/app/login-page/login-page.component.ts
+++ cluedo/src/app/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AppRoutingModule } from '../app-routing.module'; // Importa el AppRoutingModule
 
 @Component({
@@ -17,46 +18,28 @@ import { AppRoutingModule } from '../app-routing.module'; // Importa el AppRouti
 })
 export class LoginPageComponent {
 
+  constructor(private router: Router) { }
+
   password: string = '';
   username: string = '';
 
-  login(): void {
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
+  async handleLogin(): Promise<void>{
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password
+      })
+    });
+    
+    const data = await response.json();
+    if (data.success === true) {
+        this.router.navigate(['/game-user']);
+    } else {
+        alert('Usuario o contraseña incorrectos');
+    }
   }
-
-  // constructor(private http: HttpClient, private _route: ActivatedRoute) {}
-
-  // login(): void {
-  //   const url = 'http://localhost:4200/login'; // Ruta de tu endpoint de login en la API
-
-  //   // Objeto con los datos del usuario a enviar al servidor
-  //   // const body = {
-  //   //   username: username,
-  //   //   password: password
-  //   // };
-
-  //   // Realizar la petición POST al servidor
-  //   this.http.post(url, this.username, this.password).subscribe((response) => {
-  //     console.log('Respuesta del servidor:', response);
-  //     // Aquí puedes manejar la respuesta del servidor, por ejemplo, redirigir al usuario si el login fue exitoso
-  //   }, (error) => {
-  //     console.error('Error al realizar la petición:', error);
-  //     // Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
-  //   });
-
-  //}
-
-  // login() {
-  //   // Llama al método de autenticación del servicio AuthService
-  //   this.http.post(this.username, this.password)
-  //     .subscribe((result) => {
-  //       // Maneja la respuesta de la consulta a la base de datos
-  //       console.log('Respuesta de la base de datos:', result);
-  //     }, (error) => {
-  //       // Maneja cualquier error que ocurra durante la consulta a la base de datos
-  //       console.error('Error al consultar la base de datos:', error);
-  //     });
-  // }
-
 }
