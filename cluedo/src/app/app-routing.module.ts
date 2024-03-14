b28036e28bab029296a2095e75bc4b32b1b5262f
846088romeo <146849583+846088romeo@users.noreply.github.com>
2024-03-14 12:59:26 +0100

    psss

--- cluedo/src/app/app-routing.module.ts
+++ cluedo/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginPageComponent } from "./login-page/login-page.component";
 import { RegisterPageComponent } from "./register-page/register-page.component";
+import { GamePageComponent } from "./game-page/game-page.component";
 
 const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'login', component: LoginPageComponent},
-    {path: 'register', component: RegisterPageComponent}
+    {path: 'login-page', component: LoginPageComponent},
+    {path: 'register-page', component: RegisterPageComponent},
+    {path: 'game-page', component: GamePageComponent},
+    {path: '', redirectTo: 'login-page', pathMatch: 'full'}
 ];
 
 @NgModule({
@@ -14,4 +16,4 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- cluedo/src/app/login-page/login-page.component.ts
+++ cluedo/src/app/login-page/login-page.component.ts
@@ -1,40 +1,62 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module'; // Importa el AppRoutingModule
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: '../../../../../front-end-shared/css/Login/Login.css',
+
+  imports: [
+    FormsModule // Agrega FormsModule a la lista de imports
+  ]
+
 })
 export class LoginPageComponent {
 
-// password: any;
-// username: any;
-//   constructor(private http: HttpClient) {}
+  password: string = '';
+  username: string = '';
 
-//   login(username: string, password: string): void {
-//     const url = 'http://localhost:3000/login'; // Ruta de tu endpoint de login en la API
+  login(): void {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 
-//     // Objeto con los datos del usuario a enviar al servidor
-//     const body = {
-//       username: username,
-//       password: password
-//     };
+  // constructor(private http: HttpClient, private _route: ActivatedRoute) {}
 
-//     // Realizar la petición POST al servidor
-//     this.http.post(url, body).subscribe((response) => {
-//       console.log('Respuesta del servidor:', response);
-//       // Aquí puedes manejar la respuesta del servidor, por ejemplo, redirigir al usuario si el login fue exitoso
-//     }, (error) => {
-//       console.error('Error al realizar la petición:', error);
-//       // Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
-//     });
-//   }
+  // login(): void {
+  //   const url = 'http://localhost:4200/login'; // Ruta de tu endpoint de login en la API
 
-  
+  //   // Objeto con los datos del usuario a enviar al servidor
+  //   // const body = {
+  //   //   username: username,
+  //   //   password: password
+  //   // };
 
-  constructor(private _route: ActivatedRoute) {}
+  //   // Realizar la petición POST al servidor
+  //   this.http.post(url, this.username, this.password).subscribe((response) => {
+  //     console.log('Respuesta del servidor:', response);
+  //     // Aquí puedes manejar la respuesta del servidor, por ejemplo, redirigir al usuario si el login fue exitoso
+  //   }, (error) => {
+  //     console.error('Error al realizar la petición:', error);
+  //     // Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
+  //   });
+
+  //}
+
+  // login() {
+  //   // Llama al método de autenticación del servicio AuthService
+  //   this.http.post(this.username, this.password)
+  //     .subscribe((result) => {
+  //       // Maneja la respuesta de la consulta a la base de datos
+  //       console.log('Respuesta de la base de datos:', result);
+  //     }, (error) => {
+  //       // Maneja cualquier error que ocurra durante la consulta a la base de datos
+  //       console.error('Error al consultar la base de datos:', error);
+  //     });
+  // }
+
 }
