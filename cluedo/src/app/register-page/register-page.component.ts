143ee722c4c41e57cceef85692b4b68c82067b0c
846088romeo <146849583+846088romeo@users.noreply.github.com>
2024-03-16 18:42:35 +0100

    register (visual)

--- cluedo/src/app/register-page/register-page.component.ts
+++ cluedo/src/app/register-page/register-page.component.ts
@@ -1,15 +1,49 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register-page.component.html',
   styleUrls: ['../../../../../front-end-shared/css/Login/Login.css',
     '../../../../../front-end-shared/css/Login/CreateUser.css']
 
 })
 export class RegisterPageComponent {
-  constructor(private _route: ActivatedRoute) {}
+  constructor(private router : Router) {}
+
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  async handleRegister(): Promise<void> {
+    if (this.password !== this.confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    const response = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password,
+      }),
+    });
+
+    const data = await response.json();
+    if (data.success === true) {
+      this.router.navigate(['/game-page']);
+    } else {
+      alert('Usuario ya registrado');
+    }
+  }
+
+
 }
