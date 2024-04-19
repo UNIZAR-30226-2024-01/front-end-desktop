42458d6ea49895fa73c64fd4bb379ca7f923d806
Guillermo Torres Pérez <60155590+GuilleTorresPerez@users.noreply.github.com>
2024-04-19 17:01:28 +0200

    Crear componente character-selection

---
+++ cluedo/src/app/game-page/character-selection/character-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-selection.component.html',
+  styleUrl: '../../../../../../front-end-shared/css/Game/CharacterSelection.css'
+})
+export class CharacterSelectionComponent {
+
+}
