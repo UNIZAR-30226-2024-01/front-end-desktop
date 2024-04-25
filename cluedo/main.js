12fd6069f938750a539ff787fa130162718f7f67
846088romeo <146849583+846088romeo@users.noreply.github.com>
2024-04-25 17:53:22 +0200

    ya se puede hacer en app de escritorio

--- cluedo/main.js
+++ cluedo/main.js
@@ -15,7 +15,7 @@ const createWindow = () => {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('dist/index.html')
+  mainWindow.loadFile('dist/browser/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
