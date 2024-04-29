965dd3632ea0e8b561139a192fb9b2e0a2c0eee4
Pablo Moreno <841972@unizar.es>
2024-04-29 18:35:45 +0200

    empezar checkGame (error)

--- cluedo/src/app/game-page/game-page.component.ts
+++ cluedo/src/app/game-page/game-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TarjetaComponent } from '../tarjeta/tarjeta.component';
 import { ChatComponent } from '../chat/chat.component';
@@ -10,8 +10,12 @@ import { CartasDesplegableComponent } from '../cartas-desplegable/cartas-despleg
 import { TurnoComponent } from '../turno/turno.component';
 import { MainTableroComponent } from './main-tablero/main-tablero.component';
 import { CharacterSelectionComponent } from './character-selection/character-selection.component';
+import { SocketService } from '../servicios/servicio-socket/socket.service';
 import { Router } from '@angular/router';
 import { GameService } from '../servicios/servicio-game/game.service';
+import { environment } from "../../environments/environment"; 
+
+const BACKEND_URL = environment.apiUrl;
 
 @Component({
   selector: 'app-game-page',
@@ -29,8 +33,20 @@ import { GameService } from '../servicios/servicio-game/game.service';
   templateUrl: './game-page.component.html',
   styleUrl: './../../../../../front-end-shared/css/Game/Game.css',
 })
-export class GamePageComponent {
-  constructor(private router: Router, public gameService: GameService) { }
+export class GamePageComponent implements OnInit{
+  idGame: string | undefined;
+
+  constructor(private router: Router, public gameService: GameService, private socketService: SocketService) { }
+  ngOnInit() {
+    this.idGame = localStorage.getItem('partida_actual') ?? undefined;
+    if (this.idGame === undefined) {
+      this.router.navigate(['/home-page']);
+    }
+    this.checkGameExists();
+    this.socketService.auth.username = this.gameService.getUsername() ?? 'anonymous';
+    this.socketService.auth.group = this.idGame ?? this.gameService. ?? '0';
+    this.socketService.connect();
+  }
   title = 'cluedo';
 
   personajes: string[] = ["mr SOPER", "mr REDES", "mr PROG", "mr FISICA", "mr DISCRETO", "mr IA"];
@@ -47,6 +63,24 @@ export class GamePageComponent {
   //Funcion que maneja el evento de ok de los dados
   finDados(event: number): void {
     this.gameService.siguienteTurno();
+  }
+  checkGameExists() {
+    console.log('Checking if game exists');
+    const url = BACKEND_URL + '/getGame?idGame=' + this.idGame;
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json()).then((data) => {
+      const am_i_in = data.areAvailable.includes((this.gameService.getUsername()));
+      if (data.exito === true || am_i_in) {
+        console.log('Game exists');
+      } else {
+        alert('No se ha podido unirse a la partida. Inténtalo de nuevo.');
+        this.router.navigate(['/']);
+      }
+    });
   }
 
   
