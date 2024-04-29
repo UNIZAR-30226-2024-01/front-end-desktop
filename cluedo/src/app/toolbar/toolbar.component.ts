5a82bb3c434cdd81056005f6911329f416730ff4
Pablo Moreno <841972@unizar.es>
2024-04-29 09:09:38 +0200

    leave game

--- cluedo/src/app/toolbar/toolbar.component.ts
+++ cluedo/src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { Router, RouterLink } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
@@ -7,18 +8,21 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule,MatIconModule,MatMenuModule,CommonModule],
+  imports: [MatToolbarModule,MatIconModule,MatMenuModule,CommonModule,RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrl: '../../../../../front-end-shared/css/Game/NavbarGame.css'
 })
 export class ToolbarComponent {
+constructor(private router: Router/*, private http: HttpClient*/)  {
+  // Inicializar el usuario aquí si es necesario
+  // this.user=
+}
+abandonarPartida() {
+  
+  this.router.navigate(['/home-page']);
+}
   isOpen: boolean = false;
   user: string | undefined;
-
-  constructor() {
-    // Inicializar el usuario aquí si es necesario
-    // this.user=
-  }
 
   toggleMenu() {
     this.isOpen = !this.isOpen;
