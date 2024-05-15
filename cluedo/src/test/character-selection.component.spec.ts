f947d60dffa456e67a40557a15bd654b1e3eb910
analanga <126185969+analanga@users.noreply.github.com>
2024-05-15 17:01:17 +0200

    nuevo

--- cluedo/src/test/character-selection.component.spec.ts
+++ cluedo/src/test/character-selection.component.spec.ts
@@ -1,27 +1,3 @@
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CharacterSelectionComponent } from '../app/game-page/character-selection/character-selection.component';
-
-describe('CharacterSelectionComponent', () => {
-  let component: CharacterSelectionComponent;
-  let fixture: ComponentFixture<CharacterSelectionComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CharacterSelectionComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(CharacterSelectionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-*/
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CharacterSelectionComponent } from '../app/game-page/character-selection/character-selection.component';
 import { GameService } from '../app/servicios/servicio-game/game.service';
@@ -60,7 +36,11 @@ describe('CharacterSelectionComponent', () => {
   });
 
   it('should check if character is selected', () => {
-    mockGameService.charactersSelected = [false, true, false];
+    spyOn(component, 'isCharacterSelected').and.callFake((i) => {
+      const charactersSelected = [false, true, false];
+      return charactersSelected[i];
+    });
+
     expect(component.isCharacterSelected(1)).toBe(true);
     expect(component.isCharacterSelected(0)).toBe(false);
   });
